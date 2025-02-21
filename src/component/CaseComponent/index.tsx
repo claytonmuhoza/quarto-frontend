230a880da74b6d0fa97cd57c5cd93ec7be97c7d9
MUHOZA Clayton <muhozaclayton@gmail.com>
2025-02-21 20:03:13 +0100

    add stack in readme

--- src/component/CaseComponent/index.tsx
+++ src/component/CaseComponent/index.tsx
@@ -1,9 +1,9 @@
 import { Piece } from "../../Model/Piece";
 import PieceComponent from "../PieceComponent";
-
+import './style.css'
 export default function CaseComponent({piece, setGivenPiece}:{piece:Piece|null, setGivenPiece: (piece: Piece | null) => void}){
     return (
-        <div>
+        <div >
             {
                 piece ? <PieceComponent piece={piece} setGivenPiece={setGivenPiece}/> : ""
             }
